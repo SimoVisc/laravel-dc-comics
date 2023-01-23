4b92c5a34c28c7f18e04d648e972d26199bdda47
SimoVisc <simonaviscardivs@gmail.com>
2023-01-23 15:41:12 +0100

    Aggiunge tabella index

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app');
+
+@section('page-title')
+    Comics List
+@endsection
+
+@section('page-content')
+    <h1> Comics List </h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Thumb</th>
+                <th scope="col">Title</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @foreach ($comics as $comic)
+                <tr>
+                    <td>{{ $comic->id }}</td>
+                    <td> <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}" width="150"></td>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td> <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-info"> Read More</a></td>
+                </tr>
+            @endforeach
+
+
+        </tbody>
+    </table>
+@endsection
